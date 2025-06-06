66c790eb73331d79cc427bffc48eecdb843e1552
RaghuvarmaNallapu <raghuvarmanallapu447@gmail.com>
2025-06-06 07:42:21 +0530

    changes on june06th

--- src/main/java/com/example/UberReviewService/models/review.java
+++ src/main/java/com/example/UberReviewService/models/review.java
@@ -1,16 +1,30 @@
 package com.example.UberReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 
+import java.util.Date;
+
 @Entity
+@Table(name = "bookingreview")
 public class review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    Date createdAt;
+
+    @Column(nullable = false)
+    Date updatedAt;
+
+
+
 }
