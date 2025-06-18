af6e38b9677127c1c6ea7f93dfeae752fe72fd12
RaghuvarmaNallapu <raghuvarmanallapu447@gmail.com>
2025-06-18 22:49:56 +0530

    comminting changes

--- src/main/java/com/example/UberReviewService/models/BaseModel.java
+++ src/main/java/com/example/UberReviewService/models/BaseModel.java
@@ -1,6 +1,7 @@
 package com.example.UberReviewService.models;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
